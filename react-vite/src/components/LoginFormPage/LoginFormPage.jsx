5e816a21c9c15a0ab3fbf3912de74dafd53e6ad5
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-09-03 03:44:20 -0700

    Add log in styling

--- react-vite/src/components/LoginFormPage/LoginFormPage.jsx
+++ react-vite/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { thunkLogin } from "../../redux/session";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import girlCheckList from '../../../images/girlCheckList.jpg';
 import "./LoginForm.css";
 
 function LoginFormPage() {
@@ -12,7 +13,7 @@ function LoginFormPage() {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
 
-  if (sessionUser) return <Navigate to="/" replace={true} />;
+  // if (sessionUser) return <Navigate to="/${}" replace={true} />;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,40 +27,75 @@ function LoginFormPage() {
 
     if (serverResponse) {
       setErrors(serverResponse);
-    } else {
-      navigate("/");
-    }
+    } 
+    // else {
+    //   navigate("/");
+    // }
+    console.log(errors)
   };
 
+  useEffect(() => {
+    if (sessionUser) {
+      navigate(`/${sessionUser.id}/today`);
+    }
+  }, [sessionUser, navigate]);
+
+
   return (
-    <>
-      <h1>Log In</h1>
-      {errors.length > 0 &&
-        errors.map((message) => <p key={message}>{message}</p>)}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <button type="submit">Log In</button>
-      </form>
-    </>
+    <div className="login-page-container">
+      <div className="login-page-subcontainer">
+        <div>
+          <NavLink to="/" className='signup-logo'>TaskingIt</NavLink>
+        </div>
+        <div className="signup-content">
+          <div className="login-form-content">
+            <h1>Log In</h1>
+            <div className="signup-errors-container">
+              {errors.length > 0 &&
+                errors.map((message) => <p key={message}>{message}</p>)}
+            </div>
+            <form onSubmit={handleSubmit} className="signup-form">
+              <div>
+                <div className="signup-input-container">
+                  <label>
+                    Email
+                    <input
+                      type="text"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      />
+                  </label>
+                </div>
+                <div className="signup-errors-container">
+                  {errors.email && <p>{errors.email}</p>}
+                </div>
+              </div>
+              <div>
+                <div className="signup-input-container">
+                  <label>
+                    Password
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      />
+                  </label>
+                </div>
+                <div className="signup-errors-container">
+                  {errors.password && <p>{errors.password}</p>}
+                </div>
+              </div>
+              <button type="submit" className="signup-button">Log In</button>
+            </form>
+          </div>
+          <div className="signup-image-container">
+            <img src={girlCheckList} className="signup-image"/>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
