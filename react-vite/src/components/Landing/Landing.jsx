5e8508616808c1b3753615c17c5c31be5f1aa604
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-10-09 23:22:44 -0700

    Edit landing image

--- react-vite/src/components/Landing/Landing.jsx
+++ react-vite/src/components/Landing/Landing.jsx
@@ -1,19 +1,26 @@
 import './Landing.css'
-import checklistImage from '../../../images/checklist.jpg';
+// import checklistImage from '../../../images/checklist.jpg';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+// import gradient from '../../../images/background-gradient.png';
+import landing from '../../../images/landing.png';
+// import { useEffect } from 'react';
 
 export default function Landing() {
     const sessionUser = useSelector((state) => state.session.user);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (sessionUser) {
-          navigate(`/${sessionUser.id}/today`);
-        }
-      }, [sessionUser, navigate]);
+    // useEffect(() => {
+    //     if (sessionUser) {
+    //       navigate(`/${sessionUser.id}/today`);
+    //     }
+    // }, [sessionUser, navigate]);
+
+    if (sessionUser) {
+        navigate(`/${sessionUser.id}/today`);
+    }
+
     return (
         <div className="landing-container">
             <div className='landing-subcontainer'>
@@ -23,7 +30,7 @@ export default function Landing() {
                     <NavLink className="landing-link" to="/signup">Join us!</NavLink>
                 </div>
                 <div className="landing-right-side">
-                    <img src={checklistImage} className="landing-image"/>
+                    <img src={landing} className="landing-image"/>
                 </div>
             </div>
         </div>
