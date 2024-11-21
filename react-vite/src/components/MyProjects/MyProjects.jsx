91af6aa23fd10c878893199055b766d7264d436c
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-11-20 23:57:22 -0800

    Add My Projects component content

--- react-vite/src/components/MyProjects/MyProjects.jsx
+++ react-vite/src/components/MyProjects/MyProjects.jsx
@@ -1,10 +1,34 @@
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { HiHashtag } from "react-icons/hi2";
+
 import './MyProjects.css'
 
 export default function MyProjects() {
+  const user = useSelector((state) => state.session.user);
+  const projectsObj = useSelector((state) => state.projectState.projects)
+  const projects = Object.values(projectsObj)
 
     return (
       <>
-        <h1>My Projectsssssssssssssssssssss</h1>
+        <h1>My Projects</h1>
+
+        <div>
+          <p>{`${projects.length-1} projects `}</p>
+        </div>
+        <div>
+                {projects.map( project => (
+                    project.name !== 'Inbox' ? (
+                        <div key={project.id}>
+                            <NavLink to={`/${user?.id}/projects/${project.id}`}>
+                                <HiHashtag style={{color:`${project.color}`, fontSize: '16px'}}/>
+                                {project.name}
+                            </NavLink>
+                        </div>
+
+                    ): null
+                ))}
+            </div>
       </>
     );
   }
