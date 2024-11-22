be6a4871b37e089c0c80f1828240e7ef466873ee
Nicole Magallanes <nbmagallanesflores@gmail.com>
2024-11-21 23:02:45 -0800

    Add search logic

--- react-vite/src/components/MyProjects/MyProjects.jsx
+++ react-vite/src/components/MyProjects/MyProjects.jsx
@@ -3,32 +3,44 @@ import { NavLink } from 'react-router-dom';
 import { HiHashtag } from "react-icons/hi2";
 
 import './MyProjects.css'
+import { useState } from 'react';
 
 export default function MyProjects() {
+  const [searchWord, setSearchWord] = useState('');
+
   const user = useSelector((state) => state.session.user);
   const projectsObj = useSelector((state) => state.projectState.projects)
-  const projects = Object.values(projectsObj)
+  const projects = Object.values(projectsObj).filter(project => project.name.toLowerCase().includes(searchWord.toLowerCase()))
+  console.log(projects)
 
-    return (
-      <>
-        <h1>My Projects</h1>
 
-        <div>
-          <p>{`${projects.length-1} projects `}</p>
-        </div>
-        <div>
-                {projects.map( project => (
-                    project.name !== 'Inbox' ? (
-                        <div key={project.id}>
-                            <NavLink to={`/${user?.id}/projects/${project.id}`}>
-                                <HiHashtag style={{color:`${project.color}`, fontSize: '16px'}}/>
-                                {project.name}
-                            </NavLink>
-                        </div>
 
-                    ): null
-                ))}
-            </div>
-      </>
-    );
-  }
+  return (
+    <div className='my-projects-page-container'>
+      <h1 className='my-projects-title'>My Projects</h1>
+
+      <div>
+        <input
+          className="projects-search"
+          type="text"
+          placeholder="Search projects"
+          onChange={(e) => setSearchWord(e.target.value)}
+        />
+      </div>
+      <div>
+        <p>{`${projects.length-1} projects `}</p>
+          {projects.map( project => (
+              project.name !== 'Inbox' ? (
+                  <div key={project.id}>
+                      <NavLink to={`/${user?.id}/projects/${project.id}`}>
+                          <HiHashtag style={{color:`${project.color}`, fontSize: '16px'}}/>
+                          {project.name}
+                      </NavLink>
+                  </div>
+
+              ): null
+          ))}
+      </div>
+    </div>
+  );
+}
